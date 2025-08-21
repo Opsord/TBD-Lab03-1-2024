1f99e10a473c955fd94e72dfcda995b03eff283d
Andres Zelaya <andres.zelaya.d@hotmail.com>
2025-08-21 01:08:24 -0400

    Small fixes

--- LAB-TBD/src/main/java/G1TBD/LABTBD/LabTbdApplication.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/LabTbdApplication.java
@@ -2,8 +2,10 @@ package G1TBD.LABTBD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "G1TBD.LABTBD.mongo")
 public class LabTbdApplication {
 
 	public static void main(String[] args) {
